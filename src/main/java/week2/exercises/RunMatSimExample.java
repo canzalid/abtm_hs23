1970cede241cb4f0f3da63aec41bd4214e7e2660
MacBook Pro di Denis Canzali <MacProDenisCanzali@student-net-hix-2531.intern.ethz.ch>
2023-09-28 17:20:45 +0200

    second version

--- src/main/java/week2/exercises/RunMatSimExample.java
+++ src/main/java/week2/exercises/RunMatSimExample.java
@@ -1,5 +1,20 @@
 package week2.exercises;
 
-public class RunMatSimExample {
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.Controler;
+
+
+public class RunMatsimExample {
+
+	public static void main(String[] args) {
+		
+		Config config = ConfigUtils.loadConfig(args[0]);
+		
+		Controler controler = new Controler(config);
+		
+		controler.run();
+
+	}
 
 }
